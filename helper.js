fd17799baf415fa5316c36501c297ada608823fa
HarryPotter0308 <dmitrydenisov56@gmail.com>
2022-06-04 12:28:31 +1000

    end

---
+++ helper.js
@@ -0,0 +1,860 @@
+Contribution: 2022-02-25 00:00
+
+Contribution: 2022-02-25 00:01
+
+Contribution: 2022-02-25 00:02
+
+Contribution: 2022-02-25 00:03
+
+Contribution: 2022-02-25 00:04
+
+Contribution: 2022-02-26 00:00
+
+Contribution: 2022-02-26 00:01
+
+Contribution: 2022-02-26 00:02
+
+Contribution: 2022-02-26 00:03
+
+Contribution: 2022-02-26 00:04
+
+Contribution: 2022-02-26 00:05
+
+Contribution: 2022-02-26 00:06
+
+Contribution: 2022-02-26 00:07
+
+Contribution: 2022-02-26 00:08
+
+Contribution: 2022-02-26 00:09
+
+Contribution: 2022-02-26 00:10
+
+Contribution: 2022-02-27 00:00
+
+Contribution: 2022-02-27 00:01
+
+Contribution: 2022-02-27 00:02
+
+Contribution: 2022-02-27 00:03
+
+Contribution: 2022-02-27 00:04
+
+Contribution: 2022-02-27 00:05
+
+Contribution: 2022-02-27 00:06
+
+Contribution: 2022-02-28 00:00
+
+Contribution: 2022-02-28 00:01
+
+Contribution: 2022-02-28 00:02
+
+Contribution: 2022-02-28 00:03
+
+Contribution: 2022-03-01 00:00
+
+Contribution: 2022-03-01 00:01
+
+Contribution: 2022-03-01 00:02
+
+Contribution: 2022-03-01 00:03
+
+Contribution: 2022-03-01 00:04
+
+Contribution: 2022-03-01 00:05
+
+Contribution: 2022-03-01 00:06
+
+Contribution: 2022-03-02 00:00
+
+Contribution: 2022-03-02 00:01
+
+Contribution: 2022-03-02 00:02
+
+Contribution: 2022-03-02 00:03
+
+Contribution: 2022-03-03 00:00
+
+Contribution: 2022-03-03 00:01
+
+Contribution: 2022-03-03 00:02
+
+Contribution: 2022-03-03 00:03
+
+Contribution: 2022-03-03 00:04
+
+Contribution: 2022-03-03 00:05
+
+Contribution: 2022-03-03 00:06
+
+Contribution: 2022-03-03 00:07
+
+Contribution: 2022-03-03 00:08
+
+Contribution: 2022-03-03 00:09
+
+Contribution: 2022-03-04 00:00
+
+Contribution: 2022-03-04 00:01
+
+Contribution: 2022-03-04 00:02
+
+Contribution: 2022-03-04 00:03
+
+Contribution: 2022-03-04 00:04
+
+Contribution: 2022-03-04 00:05
+
+Contribution: 2022-03-04 00:06
+
+Contribution: 2022-03-04 00:07
+
+Contribution: 2022-03-04 00:08
+
+Contribution: 2022-03-04 00:09
+
+Contribution: 2022-03-05 00:00
+
+Contribution: 2022-03-05 00:01
+
+Contribution: 2022-03-05 00:02
+
+Contribution: 2022-03-05 00:03
+
+Contribution: 2022-03-05 00:04
+
+Contribution: 2022-03-05 00:05
+
+Contribution: 2022-03-06 00:00
+
+Contribution: 2022-03-07 00:00
+
+Contribution: 2022-03-07 00:01
+
+Contribution: 2022-03-07 00:02
+
+Contribution: 2022-03-07 00:03
+
+Contribution: 2022-03-07 00:04
+
+Contribution: 2022-03-08 00:00
+
+Contribution: 2022-03-08 00:01
+
+Contribution: 2022-03-08 00:02
+
+Contribution: 2022-03-08 00:03
+
+Contribution: 2022-03-08 00:04
+
+Contribution: 2022-03-08 00:05
+
+Contribution: 2022-03-08 00:06
+
+Contribution: 2022-03-09 00:00
+
+Contribution: 2022-03-09 00:01
+
+Contribution: 2022-03-09 00:02
+
+Contribution: 2022-03-09 00:03
+
+Contribution: 2022-03-09 00:04
+
+Contribution: 2022-03-09 00:05
+
+Contribution: 2022-03-09 00:06
+
+Contribution: 2022-03-11 00:00
+
+Contribution: 2022-03-12 00:00
+
+Contribution: 2022-03-12 00:01
+
+Contribution: 2022-03-12 00:02
+
+Contribution: 2022-03-12 00:03
+
+Contribution: 2022-03-12 00:04
+
+Contribution: 2022-03-12 00:05
+
+Contribution: 2022-03-12 00:06
+
+Contribution: 2022-03-12 00:07
+
+Contribution: 2022-03-12 00:08
+
+Contribution: 2022-03-12 00:09
+
+Contribution: 2022-03-12 00:10
+
+Contribution: 2022-03-12 00:11
+
+Contribution: 2022-03-13 00:00
+
+Contribution: 2022-03-13 00:01
+
+Contribution: 2022-03-13 00:02
+
+Contribution: 2022-03-13 00:03
+
+Contribution: 2022-03-13 00:04
+
+Contribution: 2022-03-13 00:05
+
+Contribution: 2022-03-13 00:06
+
+Contribution: 2022-03-13 00:07
+
+Contribution: 2022-03-13 00:08
+
+Contribution: 2022-03-14 00:00
+
+Contribution: 2022-03-14 00:01
+
+Contribution: 2022-03-14 00:02
+
+Contribution: 2022-03-14 00:03
+
+Contribution: 2022-03-14 00:04
+
+Contribution: 2022-03-14 00:05
+
+Contribution: 2022-03-15 00:00
+
+Contribution: 2022-03-15 00:01
+
+Contribution: 2022-03-15 00:02
+
+Contribution: 2022-03-15 00:03
+
+Contribution: 2022-03-15 00:04
+
+Contribution: 2022-03-15 00:05
+
+Contribution: 2022-03-15 00:06
+
+Contribution: 2022-03-15 00:07
+
+Contribution: 2022-03-15 00:08
+
+Contribution: 2022-03-15 00:09
+
+Contribution: 2022-03-16 00:00
+
+Contribution: 2022-03-16 00:01
+
+Contribution: 2022-03-16 00:02
+
+Contribution: 2022-03-16 00:03
+
+Contribution: 2022-03-16 00:04
+
+Contribution: 2022-03-16 00:05
+
+Contribution: 2022-03-16 00:06
+
+Contribution: 2022-03-16 00:07
+
+Contribution: 2022-03-16 00:08
+
+Contribution: 2022-03-16 00:09
+
+Contribution: 2022-03-17 00:00
+
+Contribution: 2022-03-17 00:01
+
+Contribution: 2022-03-17 00:02
+
+Contribution: 2022-03-17 00:03
+
+Contribution: 2022-03-17 00:04
+
+Contribution: 2022-03-17 00:05
+
+Contribution: 2022-03-17 00:06
+
+Contribution: 2022-03-17 00:07
+
+Contribution: 2022-03-17 00:08
+
+Contribution: 2022-03-17 00:09
+
+Contribution: 2022-03-17 00:10
+
+Contribution: 2022-03-18 00:00
+
+Contribution: 2022-03-18 00:01
+
+Contribution: 2022-03-18 00:02
+
+Contribution: 2022-03-18 00:03
+
+Contribution: 2022-03-18 00:04
+
+Contribution: 2022-03-19 00:00
+
+Contribution: 2022-03-20 00:00
+
+Contribution: 2022-03-20 00:01
+
+Contribution: 2022-03-20 00:02
+
+Contribution: 2022-03-20 00:03
+
+Contribution: 2022-03-20 00:04
+
+Contribution: 2022-03-20 00:05
+
+Contribution: 2022-03-20 00:06
+
+Contribution: 2022-03-20 00:07
+
+Contribution: 2022-03-20 00:08
+
+Contribution: 2022-03-20 00:09
+
+Contribution: 2022-03-20 00:10
+
+Contribution: 2022-03-21 00:00
+
+Contribution: 2022-03-21 00:01
+
+Contribution: 2022-03-21 00:02
+
+Contribution: 2022-03-21 00:03
+
+Contribution: 2022-03-21 00:04
+
+Contribution: 2022-03-21 00:05
+
+Contribution: 2022-03-21 00:06
+
+Contribution: 2022-03-21 00:07
+
+Contribution: 2022-03-21 00:08
+
+Contribution: 2022-03-21 00:09
+
+Contribution: 2022-03-22 00:00
+
+Contribution: 2022-03-22 00:01
+
+Contribution: 2022-03-22 00:02
+
+Contribution: 2022-03-22 00:03
+
+Contribution: 2022-03-22 00:04
+
+Contribution: 2022-03-22 00:05
+
+Contribution: 2022-03-22 00:06
+
+Contribution: 2022-03-22 00:07
+
+Contribution: 2022-03-22 00:08
+
+Contribution: 2022-03-22 00:09
+
+Contribution: 2022-03-22 00:10
+
+Contribution: 2022-03-23 00:00
+
+Contribution: 2022-03-23 00:01
+
+Contribution: 2022-03-23 00:02
+
+Contribution: 2022-03-23 00:03
+
+Contribution: 2022-03-24 00:00
+
+Contribution: 2022-03-25 00:00
+
+Contribution: 2022-03-25 00:01
+
+Contribution: 2022-03-25 00:02
+
+Contribution: 2022-03-25 00:03
+
+Contribution: 2022-03-25 00:04
+
+Contribution: 2022-03-25 00:05
+
+Contribution: 2022-03-25 00:06
+
+Contribution: 2022-03-26 00:00
+
+Contribution: 2022-03-26 00:01
+
+Contribution: 2022-03-26 00:02
+
+Contribution: 2022-03-27 00:00
+
+Contribution: 2022-03-27 00:01
+
+Contribution: 2022-03-27 00:02
+
+Contribution: 2022-03-27 00:03
+
+Contribution: 2022-03-27 00:04
+
+Contribution: 2022-03-27 00:05
+
+Contribution: 2022-03-27 00:06
+
+Contribution: 2022-03-27 00:07
+
+Contribution: 2022-03-28 00:00
+
+Contribution: 2022-03-28 00:01
+
+Contribution: 2022-03-28 00:02
+
+Contribution: 2022-03-28 00:03
+
+Contribution: 2022-03-28 00:04
+
+Contribution: 2022-03-28 00:05
+
+Contribution: 2022-03-28 00:06
+
+Contribution: 2022-03-28 00:07
+
+Contribution: 2022-03-28 00:08
+
+Contribution: 2022-03-29 00:00
+
+Contribution: 2022-03-29 00:01
+
+Contribution: 2022-03-29 00:02
+
+Contribution: 2022-03-29 00:03
+
+Contribution: 2022-03-29 00:04
+
+Contribution: 2022-03-29 00:05
+
+Contribution: 2022-03-29 00:06
+
+Contribution: 2022-03-29 00:07
+
+Contribution: 2022-03-29 00:08
+
+Contribution: 2022-03-29 00:09
+
+Contribution: 2022-03-29 00:10
+
+Contribution: 2022-03-30 00:00
+
+Contribution: 2022-03-30 00:01
+
+Contribution: 2022-03-30 00:02
+
+Contribution: 2022-03-30 00:03
+
+Contribution: 2022-03-30 00:04
+
+Contribution: 2022-03-30 00:05
+
+Contribution: 2022-03-30 00:06
+
+Contribution: 2022-03-30 00:07
+
+Contribution: 2022-03-30 00:08
+
+Contribution: 2022-03-30 00:09
+
+Contribution: 2022-03-30 00:10
+
+Contribution: 2022-03-30 00:11
+
+Contribution: 2022-03-31 00:00
+
+Contribution: 2022-03-31 00:01
+
+Contribution: 2022-03-31 00:02
+
+Contribution: 2022-03-31 00:03
+
+Contribution: 2022-03-31 00:04
+
+Contribution: 2022-03-31 00:05
+
+Contribution: 2022-04-01 00:00
+
+Contribution: 2022-04-01 00:01
+
+Contribution: 2022-04-01 00:02
+
+Contribution: 2022-04-01 00:03
+
+Contribution: 2022-04-01 00:04
+
+Contribution: 2022-04-01 00:05
+
+Contribution: 2022-04-01 00:06
+
+Contribution: 2022-04-01 00:07
+
+Contribution: 2022-04-01 00:08
+
+Contribution: 2022-04-02 00:00
+
+Contribution: 2022-04-02 00:01
+
+Contribution: 2022-04-02 00:02
+
+Contribution: 2022-04-02 00:03
+
+Contribution: 2022-04-02 00:04
+
+Contribution: 2022-04-02 00:05
+
+Contribution: 2022-04-02 00:06
+
+Contribution: 2022-04-03 00:00
+
+Contribution: 2022-04-03 00:01
+
+Contribution: 2022-04-03 00:02
+
+Contribution: 2022-04-03 00:03
+
+Contribution: 2022-04-03 00:04
+
+Contribution: 2022-04-03 00:05
+
+Contribution: 2022-04-03 00:06
+
+Contribution: 2022-04-03 00:07
+
+Contribution: 2022-04-03 00:08
+
+Contribution: 2022-04-03 00:09
+
+Contribution: 2022-04-04 00:00
+
+Contribution: 2022-04-04 00:01
+
+Contribution: 2022-04-04 00:02
+
+Contribution: 2022-04-04 00:03
+
+Contribution: 2022-04-04 00:04
+
+Contribution: 2022-04-04 00:05
+
+Contribution: 2022-04-04 00:06
+
+Contribution: 2022-04-04 00:07
+
+Contribution: 2022-04-04 00:08
+
+Contribution: 2022-04-04 00:09
+
+Contribution: 2022-04-04 00:10
+
+Contribution: 2022-04-05 00:00
+
+Contribution: 2022-04-06 00:00
+
+Contribution: 2022-04-06 00:01
+
+Contribution: 2022-04-06 00:02
+
+Contribution: 2022-04-06 00:03
+
+Contribution: 2022-04-06 00:04
+
+Contribution: 2022-04-06 00:05
+
+Contribution: 2022-04-06 00:06
+
+Contribution: 2022-04-06 00:07
+
+Contribution: 2022-04-06 00:08
+
+Contribution: 2022-04-06 00:09
+
+Contribution: 2022-04-07 00:00
+
+Contribution: 2022-04-08 00:00
+
+Contribution: 2022-04-08 00:01
+
+Contribution: 2022-04-08 00:02
+
+Contribution: 2022-04-08 00:03
+
+Contribution: 2022-04-08 00:04
+
+Contribution: 2022-04-09 00:00
+
+Contribution: 2022-04-09 00:01
+
+Contribution: 2022-04-09 00:02
+
+Contribution: 2022-04-09 00:03
+
+Contribution: 2022-04-09 00:04
+
+Contribution: 2022-04-09 00:05
+
+Contribution: 2022-04-10 00:00
+
+Contribution: 2022-04-10 00:01
+
+Contribution: 2022-04-10 00:02
+
+Contribution: 2022-04-10 00:03
+
+Contribution: 2022-04-10 00:04
+
+Contribution: 2022-04-10 00:05
+
+Contribution: 2022-04-10 00:06
+
+Contribution: 2022-04-11 00:00
+
+Contribution: 2022-04-12 00:00
+
+Contribution: 2022-04-12 00:01
+
+Contribution: 2022-04-12 00:02
+
+Contribution: 2022-04-12 00:03
+
+Contribution: 2022-04-12 00:04
+
+Contribution: 2022-04-12 00:05
+
+Contribution: 2022-04-13 00:00
+
+Contribution: 2022-04-13 00:01
+
+Contribution: 2022-04-13 00:02
+
+Contribution: 2022-04-13 00:03
+
+Contribution: 2022-04-13 00:04
+
+Contribution: 2022-04-13 00:05
+
+Contribution: 2022-04-14 00:00
+
+Contribution: 2022-04-14 00:01
+
+Contribution: 2022-04-14 00:02
+
+Contribution: 2022-04-14 00:03
+
+Contribution: 2022-04-14 00:04
+
+Contribution: 2022-04-14 00:05
+
+Contribution: 2022-04-15 00:00
+
+Contribution: 2022-04-15 00:01
+
+Contribution: 2022-04-15 00:02
+
+Contribution: 2022-04-15 00:03
+
+Contribution: 2022-04-15 00:04
+
+Contribution: 2022-04-15 00:05
+
+Contribution: 2022-04-15 00:06
+
+Contribution: 2022-04-15 00:07
+
+Contribution: 2022-04-15 00:08
+
+Contribution: 2022-04-15 00:09
+
+Contribution: 2022-04-15 00:10
+
+Contribution: 2022-04-15 00:11
+
+Contribution: 2022-04-16 00:00
+
+Contribution: 2022-04-16 00:01
+
+Contribution: 2022-04-16 00:02
+
+Contribution: 2022-04-16 00:03
+
+Contribution: 2022-04-17 00:00
+
+Contribution: 2022-04-17 00:01
+
+Contribution: 2022-04-17 00:02
+
+Contribution: 2022-04-17 00:03
+
+Contribution: 2022-04-17 00:04
+
+Contribution: 2022-04-17 00:05
+
+Contribution: 2022-04-17 00:06
+
+Contribution: 2022-04-17 00:07
+
+Contribution: 2022-04-17 00:08
+
+Contribution: 2022-04-18 00:00
+
+Contribution: 2022-04-18 00:01
+
+Contribution: 2022-04-18 00:02
+
+Contribution: 2022-04-18 00:03
+
+Contribution: 2022-04-18 00:04
+
+Contribution: 2022-04-18 00:05
+
+Contribution: 2022-04-18 00:06
+
+Contribution: 2022-04-19 00:00
+
+Contribution: 2022-04-19 00:01
+
+Contribution: 2022-04-19 00:02
+
+Contribution: 2022-04-20 00:00
+
+Contribution: 2022-04-20 00:01
+
+Contribution: 2022-04-20 00:02
+
+Contribution: 2022-04-20 00:03
+
+Contribution: 2022-04-20 00:04
+
+Contribution: 2022-04-20 00:05
+
+Contribution: 2022-04-20 00:06
+
+Contribution: 2022-04-20 00:07
+
+Contribution: 2022-04-20 00:08
+
+Contribution: 2022-04-20 00:09
+
+Contribution: 2022-04-20 00:10
+
+Contribution: 2022-04-20 00:11
+
+Contribution: 2022-04-21 00:00
+
+Contribution: 2022-04-21 00:01
+
+Contribution: 2022-04-21 00:02
+
+Contribution: 2022-04-21 00:03
+
+Contribution: 2022-04-21 00:04
+
+Contribution: 2022-04-21 00:05
+
+Contribution: 2022-04-21 00:06
+
+Contribution: 2022-04-21 00:07
+
+Contribution: 2022-04-21 00:08
+
+Contribution: 2022-04-21 00:09
+
+Contribution: 2022-04-21 00:10
+
+Contribution: 2022-04-22 00:00
+
+Contribution: 2022-04-22 00:01
+
+Contribution: 2022-04-23 00:00
+
+Contribution: 2022-04-23 00:01
+
+Contribution: 2022-04-23 00:02
+
+Contribution: 2022-04-23 00:03
+
+Contribution: 2022-04-23 00:04
+
+Contribution: 2022-04-23 00:05
+
+Contribution: 2022-04-24 00:00
+
+Contribution: 2022-04-24 00:01
+
+Contribution: 2022-04-24 00:02
+
+Contribution: 2022-04-24 00:03
+
+Contribution: 2022-04-24 00:04
+
+Contribution: 2022-04-24 00:05
+
+Contribution: 2022-04-24 00:06
+
+Contribution: 2022-04-24 00:07
+
+Contribution: 2022-04-24 00:08
+
+Contribution: 2022-04-24 00:09
+
+Contribution: 2022-04-24 00:10
+
+Contribution: 2022-04-25 00:00
+
+Contribution: 2022-04-25 00:01
+
+Contribution: 2022-04-25 00:02
+
+Contribution: 2022-04-25 00:03
+
+Contribution: 2022-04-25 00:04
+
+Contribution: 2022-04-25 00:05
+
+Contribution: 2022-04-26 00:00
+
+Contribution: 2022-04-27 00:00
+
+Contribution: 2022-04-27 00:01
+
+Contribution: 2022-04-27 00:02
+
+Contribution: 2022-04-27 00:03
+
+Contribution: 2022-04-27 00:04
+
+Contribution: 2022-04-27 00:05
+
+Contribution: 2022-04-27 00:06
+
+Contribution: 2022-04-27 00:07
+
+Contribution: 2022-04-28 00:00
+
+Contribution: 2022-04-28 00:01
+
+Contribution: 2022-04-28 00:02
+
+Contribution: 2022-04-28 00:03
+
+Contribution: 2022-04-28 00:04
+
+Contribution: 2022-04-28 00:05
+
+Contribution: 2022-04-28 00:06
+
+Contribution: 2022-04-28 00:07
+
+Contribution: 2022-04-28 00:08
+
+Contribution: 2022-04-28 00:09
+
